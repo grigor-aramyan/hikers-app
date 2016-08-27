68877ca5eee4c9f446e949ee02084149cffdbc4d
Davit Kk <Davit Kk>
2016-08-27 16:58:41 +0400

    davit

--- app/src/main/java/com/example/hikernotes/adapters/CommentsListAdapter.java
+++ app/src/main/java/com/example/hikernotes/adapters/CommentsListAdapter.java
@@ -47,58 +47,6 @@ public class CommentsListAdapter extends RecyclerView.Adapter<CommentsListAdapte
         this.authors = authors;
         this.comments = comments;
 
-        /*StringRequest request = new StringRequest(Request.Method.POST, MainActivity.sUrlForConnectivityCheck, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                if (response.startsWith("got it")) {
-                    StringRequest stringRequest = new StringRequest(Request.Method.POST, MainActivity.sUrlForPullingComments, new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(String response) {
-                            if (response.startsWith("no comments")) {
-                                // no comments for this tour
-                                Toast.makeText(context, "No comments for this tour!", Toast.LENGTH_LONG).show();
-                                return;
-                            }
-                            String[] comments_str = response.split(":::");
-                            String[] tmp;
-                            for (int i = 0; i < (comments_str.length - 1); i++) {
-                                String s = comments_str[i];
-                                tmp = s.split("--");
-                                authors.add(tmp[0]);
-                                comments.add(tmp[1]);
-                            }
-                        }
-                    }, new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-                            Toast.makeText(context, "Something wrong with retrieving comments! Sorry!", Toast.LENGTH_LONG).show();
-                            return;
-                        }
-                    }) {
-                        @Override
-                        protected Map<String, String> getParams() throws AuthFailureError {
-                            Map<String, String> params = new HashMap<>();
-                            params.put("tourid", tour_id + "");
-                            return params;
-                        }
-
-                        @Override
-                        public Map<String, String> getHeaders() throws AuthFailureError {
-                            Map<String, String> params = new HashMap<>();
-                            params.put("Content-Type", "application/x-www-form-urlencoded");
-                            return params;
-                        }
-                    };
-                    mQueue.add(stringRequest);
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(context, "Check your net access, please!!", Toast.LENGTH_LONG).show();
-            }
-        });
-        mQueue.add(request);*/
     }
 
     @Override
@@ -110,6 +58,7 @@ public class CommentsListAdapter extends RecyclerView.Adapter<CommentsListAdapte
     @Override
     public void onBindViewHolder(CommentViewHolder holder, int position) {
         int real_position = comments.size() - position - 1;
+
         holder.name_txt.setText(authors.get(real_position));
         holder.comment_txt.setText(comments.get(real_position));
 
